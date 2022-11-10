0a4891efd87ab1fdc52a66d97686d1e4a1d15730
Andrey Shchekin <ashmind@gmail.com>
2022-11-10 23:42:09 +1300

    Add README, auto-collapse slider when meeting is selected

---
+++ README.md
@@ -0,0 +1,21 @@
+## Overview
+
+This is small SPA application designed to improve experience of reading LDM (language design meeting) notes in [dotnet/csharplang](https://github.com/dotnet/csharplang).
+
+Main goal is to provide read/unread state.
+
+## Features 
+
+* Read C# LDM notes
+* Read C# workig group notes
+* Tracking of read state (per device)
+
+Not much else, to keep the scope in check — if you miss something like "Mark all as read" or similar, please do contribute.
+
+## Contributing
+
+All contributions are welcome. Please try to follow the current standards and styles. I might tweak things before merging.
+
+## Moving into dotnet/csharplang
+
+If anyone wants to move this directly into dotnet/csharplang, I would be all for that, though tbh in that case prerendered HTML would be a better choice over the current SPA.
--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import { Alert, Layout } from 'antd';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { MainMenu } from './MainMenu';
@@ -10,11 +10,22 @@ const queryClient = new QueryClient();
 
 export const App: React.FC = () => {
   const [selectedMeetingPath, setSelectedMeetingPath] = useState<string | null>(null);
+  const [sliderCollapsed, setSliderCollapsed] = useState<boolean>(false);
+
+  useEffect(() => {
+    if (selectedMeetingPath)
+      setSliderCollapsed(true);
+  }, [selectedMeetingPath]);
 
   return <QueryClientProvider client={queryClient}>
     <RecoilRoot>
       <div className="app-layout">
-        <Sider className="app-sider" breakpoint="lg" collapsedWidth="0" width={300}>
+        <Sider className="app-sider"
+               breakpoint="lg"
+               width={300}
+               collapsedWidth={0}
+               collapsed={sliderCollapsed}
+               onCollapse={collapsed => setSliderCollapsed(collapsed)}>
           <div className="app-siderContent">
             <Alert.ErrorBoundary>
               <MainMenu onMeetingSelected={setSelectedMeetingPath} />
